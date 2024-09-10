d513c11deaab57900722dcfbd5e85de7fecc8611
최재민 <pidge2@naver.com>
2024-09-10 12:22:19 +0900

    프로젝트 배너 추가

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -7,16 +7,28 @@ interface Project {
 
 const projectsData: Project[] = [
   {
-    title: '타이틀1',
-    description: `간단 글 설명`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
+    title: '스마트팜 모니터링 플랫폼',
+    description: `환경제어, 조명제어, 양액제어, PLC제어`,
+    imgSrc: '/static/images/project/main.png',
+    href: '/404',
   },
   {
-    title: '타이틀2',
-    description: `간단 글 설명`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    title: '이동식전원공급장치 모니터링 플랫폼',
+    description: `이동식배터리, 선박, 차량, 화재방지, 배터리 상태, 위치`,
+    imgSrc: '/static/images/project/bms.jpg',
+    href: '/404',
+  },
+  {
+    title: '반도체설비 유독가스 감지, 제어 모니터링',
+    description: `천장형, 멀티형, 키오스크, 전기차 충전기`,
+    imgSrc: '/static/images/project/fmcs.jpg',
+    href: '/404',
+  },
+  {
+    title: '전기차 충전기 키오스크',
+    description: `천장형, 멀티형, 키오스크, 전기차 충전기`,
+    imgSrc: '/static/images/project/charger.jpg',
+    href: '/404',
   },
 ]
 
