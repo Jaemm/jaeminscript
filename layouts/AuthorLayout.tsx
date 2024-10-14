cc90e73f45b94ef30798d22add2efb1e2d6454d2
unknown <pidge2@naver.com>
2024-10-15 01:21:33 +0900

    about 수정

--- layouts/AuthorLayout.tsx
+++ layouts/AuthorLayout.tsx
@@ -31,18 +31,13 @@ export default function AuthorLayout({ children, content }: Props) {
               />
             )}
             <h3 className="pb-2 pt-4 text-2xl font-bold leading-8 tracking-tight">{name}</h3>
+            <div className="text-gray-500 dark:text-gray-400">1995.07</div>
             <div className="text-gray-500 dark:text-gray-400">{occupation}</div>
-            <div className="text-gray-500 dark:text-gray-400">{company}</div>
-            <div className="text-gray-500 dark:text-gray-400">
-              SK네트웍스서비스(2019.11~2021.11)
-            </div>
-            <br></br>
-            <div className="text-gray-500 dark:text-gray-400">수원과학대학교(2014.03~2019.03)</div>
+            <div className="text-gray-500 dark:text-gray-400">noah00955@gmail.com</div>
+            <div className="text-gray-500 dark:text-gray-400">github.com/jaemm</div>
             <div className="flex space-x-3 pt-6">
               <SocialIcon kind="mail" href={`mailto:${email}`} />
               <SocialIcon kind="github" href={github} />
-              <SocialIcon kind="linkedin" href={linkedin} />
-              <SocialIcon kind="x" href={twitter} />
             </div>
           </div>
           <div className="prose max-w-none pb-8 pt-8 dark:prose-invert xl:col-span-2">
